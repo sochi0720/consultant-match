a555ddb4406e5a2dece72c9dd78c36f75e88f31c
sochi0720 <so-js.share_b@docomo.ne.jp>
2021-02-01 17:58:41 +0900

    ユーザー管理機能

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :name
+  end
+      
+  with_options presence: true, format: { with: /\A[ァ-ンー－]+\z/, message: '全角カタカナを使用してください' } do
+    validates :name_kana
+  end
+      
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/, message: '半角英数字を使用してください' } do
+  end
+      
+  validates :birth_day, presence: true
+  validates :pr, presence: true
+end
